23dee372f34583111b035047330da8aed647be17
Robert Starosta <robik2k4@gmail.com>
2025-06-03 13:15:16 +0200

    WINDOWS AA

--- main.cpp
+++ main.cpp
@@ -15,23 +15,24 @@
 #include "src/atomic_vars.h"
 #include "src/connection.h"
 
-#ifdef _WIN32
-BOOL WINAPI windows_ctrl_handler(DWORD ctrl_type) {
-    if (ctrl_type == CTRL_C_EVENT || ctrl_type == CTRL_BREAK_EVENT) {
-        stop_reading(SIGINT);
-        return TRUE;
-    }
-    return FALSE;
-}
-#endif
+// #ifdef _WIN32
+// BOOL WINAPI windows_ctrl_handler(DWORD ctrl_type) {
+//     if (ctrl_type == CTRL_C_EVENT || ctrl_type == CTRL_BREAK_EVENT) {
+//         stop_reading(SIGINT);
+//         return TRUE;
+//     }
+//     return FALSE;
+// }
+// #endif
 
 int main(int argc, char **argv) {
-    // connection_init();
-    // auto& settings = SettingsManager::getInstance();
-    // std::string current_port = settings.getPort();
-    // int current_baud = settings.getBaudRate();
 
-    Gtk::Application::create();
+    #ifdef _WIN32
+    // Windows-specific initialization if needed
+    SetProcessDPIAware(); // Handle DPI awareness
+    #endif
+
+    // Gtk::Application::create();
 
     auto app = Gtk::Application::create("org.empe.application");
     return app->make_window_and_run<MainWindow>(argc, argv);  // Added argc, argv parameters
